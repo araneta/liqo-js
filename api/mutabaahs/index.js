610abfc0f242223da649719c6932fbc3cafe45be
berkedel <akhmad.hadi@gmail.com>
2016-09-05 23:18:06 +0900

    Add mutabaah api

---
+++ api/mutabaahs/index.js
@@ -0,0 +1,132 @@
+var parse = require('co-body');
+var monk = require('monk');
+var wrap = require('co-monk');
+var _ = require('koa-route');
+var koa = require('koa');
+
+var app = koa();
+
+var dbUri = process.env.MONGODB_URI || 'localhost/liqo';
+var db = monk(dbUri);
+var mutabaahs = wrap(db.get('mutabaah'));
+
+var routes = {
+
+  // GET all mutabaah
+  index: function *(next) {
+    if ('GET' != this.method) return yield next;
+
+    var list = yield mutabaahs.find({});
+    this.body = list;
+  },
+
+  // GET mutabaah by :id
+  show: function *(id, next) {
+    if ('GET' != this.method) return yield next;
+
+    var mutabaah = yield mutabaahs.findOne({_id: id});
+
+    if (!mutabaah)
+      this.throw(404, 'Mutabaah with id=' + id + ' was not found');
+
+    this.body = mutabaah;
+  },
+
+  // POST a new mutabaah
+  create: function *(next) {
+    if ('POST' != this.method) return yield next;
+
+    var body = yield parse(this);
+    if (!body.user_id)
+      this.throw(400, 'User id is required');
+
+    if (!body.date)
+      this.throw(400, 'Date is required');
+
+    var foundMutabaah = yield mutabaahs.findOne({
+      user_id: body.user_id,
+      date: body.date
+    });
+    if (foundMutabaah)
+      this.throw(409, 'Mutabaah at date=' + body.date + ' for user_id=' + body.user_id + ' is exists');
+
+    if (!body.records)
+      this.throw(400, 'Record data is required');
+
+    for (var record of body.records) {
+      if (!record.ibadah_id)
+        this.throw(400, 'Record id is required');
+
+      if (record.value == 'undefined')
+        this.throw(400, 'Record value is required');
+    }
+
+    var createdMutabaah = yield mutabaahs.insert(body);
+    if (!createdMutabaah)
+      this.throw(405, 'Mutabaah could not be created');
+
+    this.status = 201;
+    this.set('Location', this.originalUrl + '/' + createdMutabaah._id);
+    this.body = 'Done';
+  },
+
+  // PUT a new data to mutabaah by :id
+  modify: function *(id, next) {
+    if ('PUT' != this.method) return yield next;
+
+    var data = yield parse(this);
+
+    var mutabaah = yield mutabaahs.findOne({_id: id});
+    if (!mutabaah)
+      this.throw(404, 'Mutabaah with id=' + id + ' was not found');
+
+    var updatedMutabaah = yield mutabaahs.update(mutabaah, {$set: data});
+    if (!updatedMutabaah)
+      this.throw(405, 'Unable to update');
+    else
+      this.body = 'Done';
+  },
+
+  // REMOVE mutabaah by :id
+  remove: function *(id, next) {
+    if ('DELETE' != this.method) return yield next;
+
+    var mutabaah = yield mutabaahs.findOne({_id: id});
+    if (!mutabaah)
+      this.throw(404, 'Mutabaah with id=' + id + ' was not found');
+
+    var removedMutabaah = yield mutabaahs.remove(mutabaah);
+    if (!removedMutabaah)
+      this.throw(405, 'Unable to remove');
+    else
+      this.body = 'Done';
+  },
+
+  // HEAD
+  head: function *() {
+    this.set('Connection', 'Close');
+    this.status = 200;
+  },
+
+  // OPTIONS
+  options: function *() {
+    this.set('Allow', 'HEAD,GET,POST,PUT,DELETE,OPTIONS');
+    this.status = 200;
+  },
+
+  // TRACE
+  trace: function *() {
+    this.body = 'You cannot trace :)';
+  }
+};
+
+app.use(_.get('/', routes.index));
+app.use(_.get('/:id', routes.show));
+app.use(_.post('/', routes.create));
+app.use(_.put('/:id', routes.modify));
+app.use(_.delete('/:id', routes.remove));
+app.use(_.options('/', routes.options));
+app.use(_.trace('/', routes.trace));
+app.use(_.head('/', routes.head));
+
+module.exports = app;
--- app.js
+++ app.js
@@ -7,10 +7,12 @@ var port = process.env.PORT || 3000;
 var apiAuth = require('./api/auth');
 var apiUsers = require('./api/users');
 var apiIbadahs = require('./api/ibadahs');
+var apiMutabaahs = require('./api/mutabaahs');
 
 app.use(mount('/api/auth', apiAuth));
 app.use(mount('/api/users', apiUsers));
 app.use(mount('/api/ibadahs', apiIbadahs));
+app.use(mount('/api/mutabaahs', apiMutabaahs));
 
 app.listen(port);
 console.log('Server is listening to ' + port);
